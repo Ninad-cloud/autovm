892d5a09c933fbfd47a76c345ca3eced0e2a616b
ninorhce@gmail.com <ninorhce@gmail.com>
2020-10-24 15:30:26 -0700

    nw2

---
+++ chk_Connectivity.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+chk_Connectivity(){
+
+        echo -e "\nCHECKING THE CONNECTIVITY TO ALL NODE $1 \e[36m[ IN PROCESS ] \e[0m \n"
+        PING_FAILED=0
+        #Check first if all servers are pinging or not
+        for i in "${nodes[@]}"
+        do
+		echo "$i"
+		echo "connectivity"
+		ping -c2 $i || PING_FAILED=1
+        
+		if [ $PING_FAILED -gt 0 ]
+        	then
+                	echo -e "\e[31m\nPING TO $i FAILED, PLEASE VERIFY CONNECTIVITY and RESUME SCRIPT AFTER CONNECTIVITY \e[0m\n"
+                	exit
+        	fi
+        
+	done 
+        
+	echo -e "\nALL NODES ARE \e[36m[ UP & RUNNING...!! ] \e[0m \n"
+         
+
+}
