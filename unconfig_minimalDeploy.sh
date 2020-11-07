a696ee3b630b8dbd052f4f8210786935b76a6f46
ninorhce@gmail.com <ninorhce@gmail.com>
2020-11-07 02:35:03 -0800

    added unconfig nova

--- unconfig_minimalDeploy.sh
+++ unconfig_minimalDeploy.sh
@@ -257,10 +257,127 @@ unconfig_placement(){
 
 }
 
+unconfig_nova_controller(){
+
+echo -e "\n\e[36m###### [ CONTROLLER ] : UNINSTALL COMPUTE SERVICE #######\e[0m\n\n"
+	sleep 10
+
+	# Remove all the instance , before deleting actual service.
+	echo -e "\n\n\e[36m[ CONTROLLER ] : DEELETING ALL VIRTUAL MACHINES.. #### \e[0m\n"
+    
+	source ./admin-openrc
+	echo "$OS_PROJECT_DOMAIN_NAME"
+	echo "$OS_PROJECT_NAME"
+	echo "$OS_USER_DOMAIN_NAME"
+	echo "$OS_USERNAME"
+	echo "$OS_PASSWORD"
+	echo "$OS_AUTH_URL"
+	echo "$OS_IDENTITY_API_VERSION"
+	echo "$OS_IMAGE_API_VERSION"
+	
+	for instance_id in `nova list --all-tenants | awk '{ print $2 }'`;
+	do
+	    if [ "$instance_id" == "ID" ];then
+		    echo $instance_id
+		else
+			nova delete $instance_id
+		fi
+		sleep 5
+    done
+
+    sleep 10
+
+	# Delete Service, which eventually delete all the endpoints
+        echo -e "\n\e[36m[ COMPUTE_ON_CONTROLLER ] :\e[0m DELETE NOVA COMPUTE SERVICE...."
+	
+	source ./admin-openrc
+	echo "$OS_PROJECT_DOMAIN_NAME"
+	echo "$OS_PROJECT_NAME"
+	echo "$OS_USER_DOMAIN_NAME"
+	echo "$OS_USERNAME"
+	echo "$OS_PASSWORD"
+	echo "$OS_AUTH_URL"
+	echo "$OS_IDENTITY_API_VERSION"
+	echo "$OS_IMAGE_API_VERSION"
+	
+	if openstack service list | grep nova;then
+		openstack service delete nova
+	fi
+	
+	echo "Delete user nova"
+	if openstack user list | grep nova;then
+		openstack user delete nova
+	fi
+	
+	
+	# Droping the compute databases-nova_api and nova.
+	echo -e "\n\e[36m[ CONTROLLER ] :\e[0m DROPPING COMPUTE NOVA MYSQL DB...."
+	
+	drpdb=$(mysql -uroot -p$COMMON_PASS -e "SHOW DATABASES;" | grep "nova_api")
+        if [ ! -z $drpdb ];
+        then
+                mysql -u root -p$COMMON_PASS -e "DROP DATABASE nova_api;DROP USER 'nova'@'localhost';DROP USER 'nova'@'%';"
+        fi
+	drpdb=$(mysql -uroot -p$COMMON_PASS -e "SHOW DATABASES;" | grep "nova")
+        if [ ! -z $drpdb ];
+        then
+                mysql -u root -p$COMMON_PASS -e "DROP DATABASE nova;DROP USER 'nova'@'localhost';DROP USER 'nova'@'%';"
+        fi
+	
+	drpdb=$(mysql -uroot -p$COMMON_PASS -e "SHOW DATABASES;" | grep "nova_cell0")
+        if [ ! -z $drpdb ];
+        then
+                mysql -u root -p$COMMON_PASS -e "DROP DATABASE nova_cell0;DROP USER 'nova'@'localhost';DROP USER 'nova'@'%';"
+        fi
+	
+	echo "Removing Configuration from nova.conf"
+	cp /etc/nova/nova.conf.bakup /etc/nova/nova.conf
+	sleep 2
+	
+	ERROR=""
+	echo "Restart The All Services....."
+	service nova-api restart || ERROR="yes"
+	service nova-scheduler restart || ERROR="yes"
+	service nova-conductor restart || ERROR="yes"
+	service nova-novncproxy restart || ERROR="yes"
+	
+	if [ ! -z $ERROR ];then
+		echo -e "\n\n\n\e[36mERROR OCCURED in CONTROLLER_NODE, EXITING. RECTIFY ERROR \e[0m\n\n"
+		exit
+	fi
+	echo -e "\n\n\n\e[36m#####[ CONTROLLER ] : SUCCESFULLY UNDEPLOYED COMPUTE ###### \e[0m\n\n\n"
+}
+
+unconfig_nova_compute(){
+	echo -e "\n\e[36m#### [ COMPUTE1 ] : UNDEPLOYING COMPUTE SERVICE ####\e[0m\n"
+    
+	##Unconfig nova.conf file
+	ssh root@$COMPUTE1_MGT_IP << COMMANDS
+	
+	cp /etc/nova/nova.conf.bakup /etc/nova/nova.conf
+	echo "---Restarting Nova-Compute Service----"
+	echo "service nova-compute restart" 
+	service nova-compute restart
+	
+	sleep 2
+COMMANDS
+echo -e "\n\n\n\e[36m#####[ COMPUTE1 ] : SUCCESFULLY UNDEPLOYED COMPUTE ###### \e[0m\n\n\n"
+
+}
+
+unconfig_neutron_controller(){
+
+
+
+
+
+}
+
 
 #unconfig_glance
-unconfig_placement
-
+#unconfig_placement
+unconfig_nova_controller
+unconfig_nova_compute
 
 
 
